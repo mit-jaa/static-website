dcd1fd05f9f7c3550e2df63fce9a4c81a741b6a2
commissionerjgordon <43040781+commissionerjgordon@users.noreply.github.com>
2026-07-15 21:16:47 -0700

    Adding leadership page, fixing privacy policy text and summary, and fixing mobile header nav drop down and hide on scroll behavior (#38)

--- static/js/header-scroll.js
+++ static/js/header-scroll.js
@@ -1,14 +1,16 @@
 // Enable hidden nav bar
 
 const header = document.querySelector(".header");
-let lastScrollY = window.scrollY;
+let lastScrollTop = 0;
 
 window.addEventListener("scroll", () => {
-    if (lastScrollY < window.scrollY) {
-        header.classList.add("header-hidden");
-    } else {
-        header.classList.remove("header-hidden");
-    }
+  let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
 
-    lastScrollY = window.scrollY
-})
+  if (scrollTop > lastScrollTop && scrollTop > 50) {
+    header.classList.add("header-hidden");
+  } else if (scrollTop < lastScrollTop) {
+    header.classList.remove("header-hidden");
+  }
+
+  lastScrollTop = Math.max(0, scrollTop);
+});
